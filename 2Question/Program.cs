8a6a94573e6e2ba5f6a7ce2e7a63326cb0b8314f
Satchapum Krailertrat <satchapum.mail@mail.kmutt.ac.th>
2022-09-02 23:28:57 +0700

    Finish

---
+++ 2Question/Program.cs
@@ -0,0 +1,55 @@
+﻿class Project {
+
+    public static string InputLowerNumber() {
+        Console.Write("Please input Lower number : ");
+    
+        return Console.ReadLine();
+    }
+
+    public static string InputUpperNumber() {
+        Console.Write("Please input Upper number : ");
+    
+        return Console.ReadLine();
+    }
+
+    public static string InputNumber() {
+        Console.Write("Please input Lowwer number : ");
+    
+        return Console.ReadLine();
+    }
+
+    public static int ConvertStringToInt(string value) {
+        if (int.TryParse(value, out int number)) {
+            return number;
+         }
+
+       throw new Exception("Please input decimal data."); 
+    }
+
+    public static bool IsTrue(int Input, int Lower, int Upper) {
+        
+        return Input > Lower && Input < Upper ;
+    }
+
+    public static void PrintOutput(bool Check) {
+        if(Check == true) {
+        Console.WriteLine("Output = True");
+        }
+        else {
+            Console.WriteLine("Output = False");
+        }
+    }
+
+    public static void Main(string[] args) {
+        string LowerNumberText = InputLowerNumber();
+        string UpperNumberText = InputUpperNumber();
+        string InputNumberText = InputNumber();
+        int LowerNumber = ConvertStringToInt(LowerNumberText);
+        int UpperNumber = ConvertStringToInt(UpperNumberText);
+        int InputNumberInt = ConvertStringToInt(InputNumberText);
+        bool Check = IsTrue(InputNumberInt, LowerNumber, UpperNumber);
+
+        PrintOutput(Check);
+    }
+
+}
