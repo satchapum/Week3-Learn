8b2efd890961499afb0d4646555fa158fd01cc16
Satchapum Krailertrat <satchapum.mail@mail.kmutt.ac.th>
2022-09-02 20:52:53 +0700

    First

---
+++ 1Question/Program.cs
@@ -0,0 +1,33 @@
+﻿class Project {
+    
+    public static string InputNumber(int i) {
+        Console.Write("Input Number {0} : ", i+1);
+
+        return Console.ReadLine();
+    } 
+
+    public static int Summary(int[] NumberInt) {
+        int Summary = 0;
+        for(int i = 0; i <= 2; i++) { 
+            Summary = Summary + NumberInt[i];
+        }
+
+        return Summary;
+    }
+
+    public static void PrintInformation(int Answer) {
+        Console.WriteLine("Summary is : {0}", sum);
+    }
+
+    static void Main(string[] args) {
+
+        string[] NumberText = new string[3];
+        for(int i = 0; i <= 2; i++) { 
+            NumberText[i] = InputNumber(i);
+        }
+        int[] NumberInt = Array.ConvertAll(NumberText, int.Parse); 
+        int Answer = Summary(NumberInt);
+
+        PrintInformation(Answer); 
+    }
+}
