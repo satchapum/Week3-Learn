3efe433c8361fb45567433d99ac32075ceb49500
Satchapum Krailertrat <satchapum.mail@mail.kmutt.ac.th>
2022-09-03 11:05:36 +0700

    Finish

---
+++ 3Question/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program {
+
+    public static string InputNumberText() {
+        Console.Write("Input Number of pow :");
+
+        return Console.ReadLine();
+    }
+
+    public static int ConvertStringToInt(string value) {
+        int.TryParse(value, out int number);
+
+        return number;
+    }
+
+    public static int Operation(int power) {
+        if(power == 0) {
+            return 1;
+        }
+
+        return 2 * Operation(power - 1);   
+    }
+
+    public static void PrintAnswer(int Number, int Answer) {
+        Console.WriteLine("{0} Pow of 2 is {1} ",Number ,Answer);
+    }
+
+    public static void Main(string[] args) {
+        string NumberText = InputNumberText();
+        int Number = ConvertStringToInt(NumberText);
+        int Answer = Operation(Number);
+        
+        PrintAnswer(Number,Answer);
+    }
+}
